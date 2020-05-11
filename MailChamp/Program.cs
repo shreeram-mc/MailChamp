23ac5adec5a293272d40517713d819eff4c8fec8
Shreeram <urshree@gmail.com>
2020-05-11 18:02:16 +0200

    Add project files.

---
+++ MailChamp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MailChamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
